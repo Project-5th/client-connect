627a29a360cba4a3d35284740e4f4ecf7984fcca
Naman Agarwal <naman.ag41402@gmail.com>
2022-11-28 18:19:00 +0530

    stripe and cloudinary changes done

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const mainRoutes = require("./routes/mainroutes");
 const postRoutes = require("./routes/postRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
 // const keys = require("../keys");
+const fileUpload = require("express-fileupload");
 require("dotenv").config();
 const CookieSession = require("cookie-session");
 const ejs = require("ejs");
@@ -16,12 +17,17 @@ const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("../public"));
+// app.use(express.static("../public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 //new added by me
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    limits: { fileSize: 50 * 2024 * 1024 },
+  })
+);
 app.use(
   CookieSession({
     maxAge: 24 * 60 * 60 * 1000,
--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -3,25 +3,51 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 const User = require("../models/userModel");
+const cloudinary = require("../utils/cloudinary");
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    var dir = "../public/img/posts";
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-    cb(null, dir);
-  },
-  filename: (req, file, cb) => {
-    const ext = file.mimetype.split("/")[1];
-    console.log(file);
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
+exports.upload = async (req, res, next) => {
+  const file = req.files.image;
+  let imageBuffer = [];
+  console.log(req);
+  for (let i = 0; i < file.length; i++) {
+    const result = await cloudinary.uploader.upload(file[i].tempFilePath, {
+      public_id: `${Date.now()}`,
+      folder: "images",
+    });
+    imageBuffer.push({
+      url: result.url,
+    });
+  }
+  const user = await User.findById(res.locals.id);
+  const post = await Post.findById(req.query.post);
+  // console.log(result);
+  post.image = imageBuffer;
+  // req.files.forEach((file) => {
+  //   // user.posts;
+  //   post.image.push(file.filename);
+  // });
+  await post.save();
+  next();
+};
 
-exports.upload = multer({
-  storage: multerStorage,
-}).array("image", 5);
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     var dir = "../public/img/posts";
+//     if (!fs.existsSync(dir)) {
+//       fs.mkdirSync(dir);
+//     }
+//     cb(null, dir);
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = file.mimetype.split("/")[1];
+//     console.log(file);
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   },
+// });
+
+// exports.upload = multer({
+//   storage: multerStorage,
+// }).array("image", 5);
 
 exports.newPost = async (req, res, next) => {
   try {
